123bb4256347bc28982146ae7c2a234de379c237
DIEGO FABRICIO CADENA COLEM <diego.cadena@inacapmail.cl>
2019-06-21 13:14:27 -0400

    Cambios en WebService, insercion de index.php con html basico y css, javascript de plantilla

--- ws/wsJSONRegistroMobil.php
+++ ws/wsJSONRegistroMobil.php
@@ -1,16 +1,29 @@
 <?PHP
-$hostname_localhost="localhost";
+//hostname_localhost="localhost";
+$hostname_localhost="localhost:3308";
 $database_localhost="controlcamion";
 $username_localhost="root";
-$password_localhost="";
+//$password_localhost="";
+$password_localhost="57706897";
+
 $conexion=mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
 	$patente = $_POST["patente"];
 	$sello = $_POST["sello"];
 	$fecha = $_POST["fecha"];
 	$imagen = $_POST["imagen"];
+	//$path = dirname($_SERVER['PHP_SELF'],2)."/imagenes/$sello.jpg";
 	$path = "imagenes/$sello.jpg";
-	$url = "http://$hostname_localhost/RegistroCamion/$path";
-	$url = "imagenes/".$sello.".jpg";
+	$url = "http://$hostname_localhost/PaginaRegistroCamion/$path";
+	//$url = "imagenes/".$sello.".jpg";
+	$path = dirname(__DIR__)."/imagenes/";
+	
+	if (!is_dir($path)) 
+	{
+		mkdir($path);
+	}
+	
+	$path = dirname(__DIR__)."/imagenes/$sello.jpg";
+	
 	file_put_contents($path,base64_decode($imagen));
 	$bytesArchivo=file_get_contents($path);
 	$sql="INSERT INTO tbcontrolcamion (patente,sello,fecha,imagen,url) VALUES (?,?,?,?,?)";
@@ -20,7 +33,7 @@ $conexion=mysqli_connect($hostname_localhost,$username_localhost,$password_local
 	if($stm->execute()){
 		echo "registra";
 	}else{
-		echo "noRegistra";
+		echo "noRegistra" ;
 	}
 	
 	mysqli_close($conexion);
